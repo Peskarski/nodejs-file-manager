7d648cb4ea74adefbc8f40d731fd17a7652676b0
v.peskarski <v.peskarskij@gmail.com>
2022-12-15 17:27:37 +0100

    Revert "feat: implement navigation and files functions"
    This reverts commit 4c67e05beb13bd0fa0216969bc4ef7364e60925c.

--- src/commands/navigation/up.js
+++
@@ -1,7 +0,0 @@
-import os from 'node:os';
-
-export const goToUpperDirectory = () => {
-    if (process.cwd() !== os.homedir()) {
-        process.chdir('../');
-    }
-}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import os from 'node:os';
-import start from './scripts/start.js';
-import { readline } from './readline/index.js';
-
-const homeDirectory = os.homedir();
-process.chdir(homeDirectory);
-
-start();
-readline();
